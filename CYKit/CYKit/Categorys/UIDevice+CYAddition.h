1f8661e18850b62d77e7dba901ea26bfe226cdc3
cheng.yan <cheng.yan@alibaba-inc.com>
2017-08-31 18:29:03 +0800

    add UIDevice+CYAddition category file

--- CYKit/CYKit/Categorys/UIDevice+CYAddition.h
+++ CYKit/CYKit/Categorys/UIDevice+CYAddition.h
@@ -10,4 +10,35 @@
 
 @interface UIDevice (CYAddition)
 
+///-------------------------------------------
+/// @name device infos
+///-------------------------------------------
+
+/**
+ machine name ,eg. iPhone2,1 => iPhone3GS iPhone3,1 => iPhone4
+
+ @return machine name, not iPhone 4,iPhone 3GS, otherwise iPhone2,1 iPhone3,1
+ */
++ (NSString *)cy_deviceName;
+
+/**
+ ip address, should connect with wifi or 3g,4g net
+
+ @return device current ip address
+ */
++ (NSString *)cy_ipAddress;
+
+/**
+ mac address, ios 10 or later may be nil
+
+ @return mac address
+ */
++ (NSString *)cy_macAddress;
+
+/**
+ carrier name ,format => carrier name + country code
+
+ @return carrier name
+ */
++ (NSString *)cy_carrier;
 @end
--- CYKit/CYKit/Categorys/UIDevice+CYAddition.m
+++ CYKit/CYKit/Categorys/UIDevice+CYAddition.m
@@ -7,7 +7,136 @@
 //
 
 #import "UIDevice+CYAddition.h"
+#import <CoreTelephony/CTCarrier.h>
+#import <CoreTelephony/CTTelephonyNetworkInfo.h>
+
+#include <sys/socket.h> // Per msqr
+#include <sys/sysctl.h>
+#include <net/if.h>
+#include <net/if_dl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <ifaddrs.h>
+#include <arpa/inet.h>
 
 @implementation UIDevice (CYAddition)
++ (NSString *)cy_deviceName
+{
+    size_t size;
+    
+    // Set 'oldp' parameter to NULL to get the size of the data
+    // returned so we can allocate appropriate amount of space
+    sysctlbyname("hw.machine", NULL, &size, NULL, 0);
+    
+    // Allocate the space to store name
+    char *name = malloc(size);
+    
+    // Get the platform name
+    sysctlbyname("hw.machine", name, &size, NULL, 0);
+    
+    // Place name into a string
+    NSString *machine = [NSString stringWithUTF8String:name];
+    
+    // Done with this
+    free(name);
+    
+    return machine;
+}
 
++ (NSString *)cy_carrier
+{
+    if(NSClassFromString(@"CTTelephonyNetworkInfo")){
+        @try {
+            /*  25303 */
+            CTTelephonyNetworkInfo *networkInfo = [[CTTelephonyNetworkInfo alloc] init];
+            CTCarrier *carrier = [networkInfo subscriberCellularProvider];
+            if ([[carrier carrierName] length] > 0) {
+                return [NSString stringWithFormat:@"%@_%@%@", [carrier carrierName], [carrier mobileCountryCode], [carrier mobileNetworkCode]];
+            }else{
+                return nil;
+            }
+        }
+        @catch (NSException *exception) {
+            NSLog(@"%@",exception);
+        }
+        @finally {
+            ;
+        }
+    }
+    return nil;
+}
+
++ (NSString *)cy_ipAddress
+{
+    NSString *address = @"";
+    struct ifaddrs *interfaces = NULL;
+    struct ifaddrs *temp_addr = NULL;
+    int success = 0;
+    
+    success = getifaddrs(&interfaces);
+    
+    if (success == 0) { // 0 表示获取成功
+        temp_addr = interfaces;
+        while (temp_addr != NULL) {
+            if( temp_addr->ifa_addr->sa_family == AF_INET) {
+                // Check if interface is en0 which is the wifi connection on the iPhone
+                if ([[NSString stringWithUTF8String:temp_addr->ifa_name] isEqualToString:@"en0"]) {
+                    // Get NSString from C String
+                    address = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in  *)temp_addr->ifa_addr)->sin_addr)];
+                }
+            }
+            temp_addr = temp_addr->ifa_next;
+        }
+    }
+    freeifaddrs(interfaces);
+    
+    return address;
+}
+
++ (NSString *)cy_macAddress
+{
+    int                 mib[6];
+    size_t              len;
+    char                *buf;
+    unsigned char       *ptr;
+    struct if_msghdr    *ifm;
+    struct sockaddr_dl  *sdl;
+    
+    mib[0] = CTL_NET;
+    mib[1] = AF_ROUTE;
+    mib[2] = 0;
+    mib[3] = AF_LINK;
+    mib[4] = NET_RT_IFLIST;
+    
+    if ((mib[5] = if_nametoindex("en0")) == 0) {
+        printf("Error: if_nametoindex error\n");
+        return NULL;
+    }
+    
+    if (sysctl(mib, 6, NULL, &len, NULL, 0) < 0) {
+        printf("Error: sysctl, take 1\n");
+        return NULL;
+    }
+    
+    if ((buf = malloc(len)) == NULL) {
+        printf("Could not allocate memory. error!\n");
+        return NULL;
+    }
+    
+    if (sysctl(mib, 6, buf, &len, NULL, 0) < 0) {
+        printf("Error: sysctl, take 2");
+        free(buf);
+        return NULL;
+    }
+    
+    ifm = (struct if_msghdr *)buf;
+    sdl = (struct sockaddr_dl *)(ifm + 1);
+    ptr = (unsigned char *)LLADDR(sdl);
+    NSString *outstring = [NSString stringWithFormat:@"%02X:%02X:%02X:%02X:%02X:%02X",
+                           *ptr, *(ptr+1), *(ptr+2), *(ptr+3), *(ptr+4), *(ptr+5)];
+    free(buf);
+    
+    return outstring;
+}
 @end
