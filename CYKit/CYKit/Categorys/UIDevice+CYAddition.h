5218f481513ab43dcd3e97ba3045891c61b229b4
cheng.yan <cheng.yan@alibaba-inc.com>
2017-08-31 18:13:10 +0800

    提交gitignored文件

---
+++ CYKit/CYKit/Categorys/UIDevice+CYAddition.h
@@ -0,0 +1,13 @@
+//
+//  UIDevice+CYAddition.h
+//  Pods
+//
+//  Created by chengyan on 2017/8/31.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIDevice (CYAddition)
+
+@end
---
+++ CYKit/CYKit/Categorys/UIDevice+CYAddition.m
@@ -0,0 +1,13 @@
+//
+//  UIDevice+CYAddition.m
+//  Pods
+//
+//  Created by chengyan on 2017/8/31.
+//
+//
+
+#import "UIDevice+CYAddition.h"
+
+@implementation UIDevice (CYAddition)
+
+@end
